1c33f9da0122ef125d10b157476df2c50f332022
Tandra Malm <tandra_malm@hotmail.com>
2024-01-23 11:55:58 -0700

    changed instance in database

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,21 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+### Exercise 2: Update the first store
+
+# Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
+# Load the second store from the database and assign it to `@store2`.
+# Update the first store (`@store1`) instance in the database. (Change its name or something.)
+
 # Your code goes here ...
+@store1 = Store.find(id = 1)
+
+@store2 = Store.find(id = 2)
+
+store = Store.find_by(annual_revenue: 300000)
+
+if store
+  store.update(annual_revenue: 400000)
+else
+  puts "No store found with the annual revenue of $300,000."
+end
